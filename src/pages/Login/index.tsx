553ddd16a7dfa4d1d006f04a047be5d13ae70c5f
Henrique Rosa <rosa.henrique@outlook.com.br>
2020-08-09 19:19:26 -0300

    added base page login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Field, withTypes } from 'react-final-form';
+import { TextField } from '@material-ui/core';
+import LockIcon from '@material-ui/icons/Lock';
 
-const Login: React.FC = () => <div />;
+import {
+  DivMain,
+  Card,
+  Avatar,
+  AvatarIcon,
+  Hint,
+  FormDiv,
+  InputDiv,
+} from './styles';
+
+interface FormValues {
+  email?: string;
+  password?: string;
+}
+
+const { Form } = withTypes<FormValues>();
+
+const renderInput = ({
+  meta: { touched, error } = { touched: false, error: undefined },
+  input: { ...inputProps },
+  ...props
+}) => (
+  <TextField
+    error={!!(touched && error)}
+    helperText={touched && error}
+    {...inputProps}
+    {...props}
+    fullWidth
+  />
+);
+
+const validate = (values: FormValues) => {
+  const errors: FormValues = {};
+  if (!values.email) {
+    errors.email = 'teste';
+  }
+  if (!values.password) {
+    errors.password = 'teste';
+  }
+  return errors;
+};
+
+const Login: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const submit = (auth: FormValues) => {
+    setLoading(true);
+    console.log(auth);
+  };
+
+  return (
+    <Form
+      onSubmit={submit}
+      validate={validate}
+      render={({ handleSubmit }) => (
+        <form noValidate>
+          <DivMain>
+            <Card>
+              <Avatar>
+                <AvatarIcon>
+                  <LockIcon />
+                </AvatarIcon>
+              </Avatar>
+              <Hint>Hint: demo / demo</Hint>
+
+              <FormDiv>
+                <InputDiv>
+                  <Field
+                    autoFocus
+                    name="email"
+                    // @ts-ignore
+                    component={renderInput}
+                    disabled={loading}
+                    label="E-mail"
+                  />
+                </InputDiv>
+                <InputDiv>
+                  <Field
+                    autoFocus
+                    name="password"
+                    // @ts-ignore
+                    component={renderInput}
+                    disabled={loading}
+                    label="Senha"
+                  />
+                </InputDiv>
+              </FormDiv>
+            </Card>
+          </DivMain>
+        </form>
+      )}
+    />
+  );
+};
 
 export default Login;
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import {
+  Card as CardMaterial,
+  Avatar as AvatarMaterial,
+} from '@material-ui/core';
+
+const DivMain = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: flex-start;
+  background: url('https://source.unsplash.com/random/1600x900');
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const Card = styled(CardMaterial)`
+  min-width: 300;
+  margin-top: 6em;
+`;
+
+const Avatar = styled.div`
+  margin: 1em;
+  display: flex;
+  justify-content: center;
+`;
+
+const AvatarIcon = styled(AvatarMaterial)`
+  background: grey;
+`;
+
+const Hint = styled.div`
+  padding: 0 1em 1em 1em;
+`;
+
+const FormDiv = styled.div`
+  padding: 0 1em 1em 1em;
+`;
+
+const InputDiv = styled.div`
+  padding: 0 1em 1em 1em;
+`;
+
+export { DivMain, Card, Avatar, AvatarIcon, Hint, FormDiv, InputDiv };
